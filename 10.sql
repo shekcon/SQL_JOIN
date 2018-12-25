610a1e8ff4959bbc0d82f66da36ebecf235202cb
Shekcon <quangsang9773@gmail.com>
2018-12-25 20:43:37 +0700

    SQL joins

--- 10.sql
+++ 10.sql
@@ -1,4 +1,5 @@
 SELECT groups.name as winner_group_name FROM groups
-JOIN matches ON matches.winner_team_id = groups.id
+JOIN teams ON teams.group_id = groups.id
+JOIN matches ON matches.winner_team_id = teams.id
 WHERE matches.stage_name = 'Semi-finals'
 ORDER BY EXTRACT(HOUR FROM matches.start_at);
